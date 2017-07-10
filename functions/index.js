5dcc0d82dc5af4a6a11230594579ab4578d64161
chrisrenfrow <mail@chrisrenfrow.me>
2017-07-10 16:17:36 -0700

    Handles opening of json map from publicly hosted URL

--- functions/index.js
+++ functions/index.js
@@ -3,100 +3,17 @@
 process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').ApiAiApp;
 const functions = require('firebase-functions');
+const request = require('request');
 
-const map = {
-	'name': 'Spirtual Experience',
-	'meta': {
-		'author': 'Chris Renfrow',
-		'email': 'mail@chrisrenfrow.me',
-		'tagline': 'You wakex up in a world of darkness with a spirit as your guide. Will you make it out of the labyrinth?',
-		'welcome': 'Oh, you finally decided to wake up huh? I bet you\'re a bit confused... I\'m Ava, and you are blind and deaf. Oh! But don\'t worry, it\'s temporary, well... it could be if we can stop that scaley heathen in time. Any questions?'
-	},
-	'rooms': {
-		'room1': {
-			'alias': 'forest',
-			'bsay': 'We\'re in the middle of a forest. There\'s a clearing directly ahead of us and the mountains are to the left.',
-			'ctxHelp': 'I\'ll guide you wherever you\'d like to go and try my best to keep you out of harm\'s way. I will be your eyes and ears.',
-			'actions': null,
-			'exits': {
-				'forward': 'room2',
-				'right': '-1',
-				'back': '-1',
-				'left': 'mountains'
-			},
-			'objects': {
-				'shard': {
-					'alias': 'pulsing metallic shard',
-					'bsay': 'It\'s humming with a strange energy, I\'m not sure what to make of it but it seems like it might be important. Why not pick it up?',
-					'actions': ['take', 'examine']
-				}
-			},
-			'enemies': null
-		},
-		'room2': {
-			'alias': 'clearing',
-			'bsay': 'Oh, the sky is so beautiful! You should really see- oh, um, nevermind. There\'s an impassable steep hill in front of you past the clearing. There\'s a stone structure to the right and more mountains to the left. The forest is behind us.',
-			'ctxHelp': 'That stone structure looks important but I feel something pulling me toward the mountains...',
-			'actions': null,
-			'exits': {
-				'forward': '-1',
-				'right': 'room4',
-				'back': 'room1',
-				'left': 'room3'
-			},
-			'objects': {
-				'burnt patches': {
-					'alias': 'burnt patches in the grass',
-					'bsay': 'The grass is burned in random spots leading toward the stone structure, some spots are still smoldering...',
-					'actions': ['examine']
-				}
-			},
-			'enemies': null
-		},
-		'room3': {
-			'alias': 'mountains',
-			'bsay': 'We\'re at the foot of the mountain range. The mountains are so tall! The forest is to the left and the clearing is to the right and behind us.',
-			'ctxHelp': 'Maybe I can break that crystal out of the rock... Just ask me to grab it for you and I\'ll give it my best shot.',
-			'actions': null,
-			'exits': {
-				'forward': '-1',
-				'right': 'room2',
-				'back': 'room2',
-				'left': 'room1'
-			},
-			'objects': {
-				'crystal': {
-            	    'alias': 'crystal in the rock',
-            	    'bsay': 'There\'s a crystal embedded in the rock... Maybe I could break it out...',
-            	    'actions': ['take', 'examine']
-				}
-			},
-			'enemies': null
-		},
-		'room4': {
-			'alias': 'stone structure',
-			'description': 'These structures are ancient... To the left is the steep hill, the forest is to the right and the clearing is behind us. I think I sense- Oh! There\'s an enemy here!',
-			'contextualHelp': 'I don\'t think we should approach him until we\'ve explored more...',
-			'actions': null,
-			'exits': {
-				'forward': '-1',
-				'right': 'room1',
-				'back': 'room2',
-				'left': '-1'
-			},
-			'objects': null,
-			'enemies': {
-          	'creature': {
-            	'alias': 'reptilian creature leaning against a stone pillar',
-            	'bsay': 'It\'s the reptilian creature that cursed you! I can taste the greed seeping from them. It tastes bad.',
-					'actions': ['attack','examine']
-				}
-			}
-		}
-	}
-};
+const HOST_URL = 'https://interactive-fiction-9f0ad.firebaseapp.com/';
+const PUBLIC_URL = HOST_URL + 'public/';
+const MAPS_URL = PUBLIC_URL + 'maps/';
 
-var current = map.rooms['room1'];
+const SELECTED_MAP_URL = MAPS_URL + 'TEMPLATE.json';
+const START_ZONE = 'zone1';
+
+var map = null;
+var zone = null;
 
 exports.underwaterAdventure = functions.https.onRequest((request, response) => {
 	const app = new App({request, response});
@@ -108,12 +25,12 @@ exports.underwaterAdventure = functions.https.onRequest((request, response) => {
 	}
 
 	function help_f (app) {
-		app.ask(current.ctxHelp);
+		app.ask(zone.ctxHelp);
 	}
 
 	function attack_f (app) {
-		let target = current.enemies[app.getArgument('any')];
-		if (current.enemies != null && current.enemies[target]) {
+		let target = zone.enemies[app.getArgument('any')];
+		if (zone.enemies != null && zone.enemies[target]) {
 			app.ask('I hope this works... Attacking ' + target);
 		} else {
 			app.ask('There are no enemies here. Are you okay?');
@@ -121,10 +38,10 @@ exports.underwaterAdventure = functions.https.onRequest((request, response) => {
 	}
 
 	function navigate_f (app) {
-		let direction = current.exits[app.getArgument('Directions')];
+		let direction = zone.exits[app.getArgument('Directions')];
 		if (direction != '-1' && direction) {
-			current = map.rooms[direction];
-			app.ask('Alright, guiding you to the ' + current.alias);
+			zone = map.rooms[direction];
+			app.ask('Alright, guiding you to the ' + zone.alias);
 		} else {
 			app.ask('Uh, I don\'t see that here...');
 		}
@@ -138,34 +55,34 @@ exports.underwaterAdventure = functions.https.onRequest((request, response) => {
 		let target = app.getArgument('thing');
 		var bsay;
 		if (target == null) {
-			bsay = current.bsay;
+			bsay = zone.bsay;
 			console.log('I made it here');
-			if (current.enemies != null) {
+			if (zone.enemies != null) {
 				bsay += ' There are enemies nearby! ';
-				let e = current.enemies[target];
-    			if (e) {
-				    bsay += e + ', ';
+				let e = zone.enemies[target];
+				if (e) {
+					bsay += e + ', ';
 				}
 			}
-			if (current.objects != null) {
-    			bsay += ' There\'s also an object here, ';
-				let o = current.objects[target];
-    			if (o) {
-				    bsay += o + ', ';
+			if (zone.objects != null) {
+				bsay += ' There\'s also an object here, ';
+				let o = zone.objects[target];
+				if (o) {
+					bsay += o + ', ';
 				}
 			}
 		} else if (target != null) {
 			let t;
-			if (current.enemies != null) {
-				t = current.enemies[target];
-			    if (t != null) {
+			if (zone.enemies != null) {
+				t = zone.enemies[target];
+				if (t != null) {
 					bsay = t.bsay;
 				} else {
 					bsay = 'I don\'t see that...';
 				}
 			}
-			if (current.objects != null) {
-    			t = current.objects[target];
+			if (zone.objects != null) {
+				t = zone.objects[target];
 				if (t != null) {
 					bsay = t.bsay;
 				} else {
@@ -181,7 +98,7 @@ exports.underwaterAdventure = functions.https.onRequest((request, response) => {
 	}
 
 	function take_f (app) {
-		let target = current.objects[app.getArgument('any')];
+		let target = zone.objects[app.getArgument('any')];
 		if (target) {
 			app.ask('Alright, I\'ll grab the ' + target.alias + '...');
 		} else {
@@ -207,6 +124,17 @@ exports.underwaterAdventure = functions.https.onRequest((request, response) => {
 	actionMap.set('look', look_f);
 	actionMap.set('take', take_f);
 	// actionMap.set('interact', interact_f);
-
+	if (zone == null) {
+		request({
+			url: SELECTED_MAP_URL,
+			json: true
+		}, function (error, response, body) {
+			if (!error && response.statusCode === 200) {
+				console.log(body); // Print the json response
+			}
+			map = JSON.parse(body);
+		});
+		zone = map.zones[START_ZONE];
+	}
 	app.handleRequest(actionMap);
 });
