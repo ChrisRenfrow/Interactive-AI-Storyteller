222a4c400698375bee3c28546ebc4abe69139d87
Chris Renfrow <mail@chrisrenfrow.me>
2017-07-15 19:18:20 -0700

    Updating json

--- functions/index.js
+++ functions/index.js
@@ -3,24 +3,44 @@
 process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').ApiAiApp;
 const functions = require('firebase-functions');
+const vsprintf = require('sprintf-js').vsprintf;
 
-var STORY = './stories/avas_story';
-var world = require(STORY + '/plot.json');
-var bot = require(STORY + '/bot_info.json');
+var STORY = './stories/test_story';
+var world = require(STORY + '/world.json');
+var bot = require(STORY + '/bot.json');
 var world_dict = require(STORY + '/dictionary.json');
 var zone = world.zones['zone1'];
 
-exports.underwaterAdventure = functions.https.onRequest((request, response) => {
+// Utility function to pick prompts
+function getRandomReply (array) {
+	return (array[Math.floor(Math.random() * (array.length))]);
+}
+
+function getDefaultHelp () {
+	return (getRandomReply(bot.dialogue.actions.help.false));
+}
+
+function getContextHelp () {
+	if (zone.contex_help) {
+		return (zone.contex_help);
+	} else {
+		return (getDefaultHelp());
+	}
+}
+
+exports.interactiveStory = functions.https.onRequest((request, response) => {
 	const app = new App({request, response});
 	console.log('Request headers: ' + JSON.stringify(request.headers));
 	console.log('Request body: ' + JSON.stringify(request.body));
 
 	function welcome_f (app) {
-		app.ask(world.bot_details.welcome);
+		// app.ask(getRandomReply(bot.dialogue.conversation.welcome));
+		app.ask('Hmm?');
+
 	}
 
 	function help_f (app) {
-		app.ask(zone.ctxHelp);
+		app.ask(getContextHelp());
 	}
 
 	function navigate_f (app) {
@@ -31,9 +51,7 @@ exports.underwaterAdventure = functions.https.onRequest((request, response) => {
 		if (direction != null) {
 			if (zone.exits[direction] != null) {
 				zone = world.zones[zone.exits[direction].link];
-				app.ask('We\'re going toward ' + zone.alias);
-			} else {
-				app.ask('That\'s not there.');
+				app.ask(vsprintf(getRandomReply(bot.dialogue.actions.navigate.true), zone.alias));
 			}
 		} else if (target != null) {
 			for (let i = 0; zone.exits[i] != null; i++) {
@@ -52,9 +70,8 @@ exports.underwaterAdventure = functions.https.onRequest((request, response) => {
 	function look_f (app) {
 		let target = app.getArgument('thing');
 		let bsay = 'There\'s nothing here...';
-		console.log(JSON.stringify(zone));
 		if (target == null) {
-			bsay = zone.bsay;
+			bsay = getRandomReply(zone.bsay);
 		}
 		app.ask(bsay);
 	}
@@ -68,13 +85,29 @@ exports.underwaterAdventure = functions.https.onRequest((request, response) => {
 		}
 	}
 
+	function attack_f (app) {
+		let target = zone.entities[app.getArgument('target')];
+		app.ask(vsprintf(getRandomReply(bot.dialogue.actions.attack.true),target));
+	}
+
+	function listMeta () {
+		let meta = world.name + 'Info: ';
+		for (let e in world.meta) {
+			meta += e[0] + ': ' + e;
+		}
+		app.say(meta);
+	}
+
 	let actionMap = new Map();
 
 	actionMap.set('input.welcome', welcome_f);
-	actionMap.set('help', help_f);
-	actionMap.set('navigate', navigate_f);
-	actionMap.set('look', look_f);
-	actionMap.set('take', take_f);
+	actionMap.set('input.help', help_f);
+	actionMap.set('input.navigate', navigate_f);
+	actionMap.set('input.look', look_f);
+	actionMap.set('input.take', take_f);
+	actionMap.set('input.attack', attack_f);
+
+	actionMap.set('debug.meta', listMeta);
 
 	app.handleRequest(actionMap);
 });
---
+++ functions/stories/test_story/README.md
@@ -0,0 +1,14 @@
+## Storyteller Format
+Every storyteller needs to be able to handle any of the available interactions. These break down into two main categories.
+- #### **Actions**
+	- Inspect
+	- Navigate
+	- Attack
+	- Interact
+	- Speak
+	- etc.
+- #### **Conversation**
+	- Welcome
+	- Help
+	- Small-talk
+	- Inquiry
